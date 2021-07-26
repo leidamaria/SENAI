ec502201da9e1c6af7633c7672b9b85d0ca0d386
leidamaria <86577790+leidamaria@users.noreply.github.com>
2021-07-26 18:45:08 -0300

    Update programa3.c

--- programa3.c
+++ programa3.c
@@ -1,41 +1,126 @@
+
 /*---- 
-3. Fazer um programa que solicita um número decimal e imprime o
+3. Fazer um programa que solicita um nC:mero decimal e imprime o
 correspondente em hexa e octal. 
 Por Leida Maria
 -----*/
 
 #include <stdio.h>
 
-int calculaOctal(int num);
-int calculaHexa(int num);
-int main()
+int calculaOctal (int num);
+int calculaHexa (int num);
+char converteLetra(int num);
+
+
+int main ()
 {
-    float numero;
-    printf("Digite um número decimal: ");
-    scanf ("%f",&numero);
-    int num = numero;
-    calculaOctal(num);
-    return 0;
+  float numero;
+  printf ("Digite um numero decimal: ");
+  scanf ("%f", &numero);
+  int num = numero;
+  if (num < 8)
+    {
+      printf ("Octal equivalente: %d", num);
+    }
+  else
+    {
+      calculaOctal (num);
+    }
+  
+  if (num < 10)
+    {
+      printf ("\nHexadecimal equivalente: %d", num);
+    }
+    
+    else if (num <= 15)
+            {
+                printf("\nHexadecimal equivalente: %c", converteLetra(num));
+            }
+        else 
+            {
+                calculaHexa (num);
+            }
+  return (0);
 }
 
-int calculaOctal(int num)
+int calculaOctal (int num)
 {
-    
-    int nr_octal[10];
-    int i = 0;
-    int resul;
-    while ( num >= 8)
+
+  int nr_octal[10];
+  int i = 0;
+  int resul;
+  while (num >= 8)
     {
-        resul = num / 8;
-        int resto = num % 8; 
-        nr_octal[i] = resto;
-        num = resul;
-        i++;
+      resul = num / 8;
+      int resto = num % 8;
+      nr_octal[i] = resto;
+      num = resul;
+      i++;
     }
-    printf("Octal equivalente: %d", resul);
-    for (int x=i-1; x>=0; x--)
+  printf ("Octal equivalente: %d", resul);
+  for (int x = i - 1; x >= 0; x--)
     {
-        printf("%d", nr_octal[x]);
+      printf ("%d", nr_octal[x]);
     }
-    
+
 }
+
+int calculaHexa (int num)
+{
+
+  int nr_hexa[10];
+  int i = 0;
+  int resul;
+  while (num >= 16)
+    {
+      resul = num / 16;
+      int resto = num % 16;
+      nr_hexa[i] = resto;
+      num = resul;
+      i++;
+    }
+    if (resul<10)
+        { printf ("\nHexa equivalente: %d", resul);}
+    else
+        {      
+            char r = converteLetra(resul);
+            printf ("%c", r);n
+    for (int x = i - 1; x >= 0; x--)
+    {
+      char l = converteLetra(nr_hexa[x]);
+      printf ("%c", l);
+    }
+
+}
+
+char converteLetra(int num)
+
+{
+    char letra;
+    if (num==10)
+        {
+            letra = 'A';
+        }
+    else if (num==11)
+        {
+            letra = 'B';
+        }
+    else if (num==12)
+        {
+            letra = 'C';
+        }
+    else if (num==13)
+        {
+            letra = 'D';
+        }
+    else if (num==14)
+        {
+            letra = 'E';
+        }
+    else if (num==15)
+        {
+            letra = 'F';
+        }
+    return(letra);
+}
+
