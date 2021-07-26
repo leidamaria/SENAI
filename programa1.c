0001c529ba8bc90cc7a83bfa2ef199b74f13bc0d
leidamaria <86577790+leidamaria@users.noreply.github.com>
2021-07-26 18:32:11 -0300

    Create programa1.c

---
+++ programa1.c
@@ -0,0 +1,17 @@
+/----
+1. Fazer um programa em C que pergunta um valor em metros e imprime o
+correspondente em decímetros, centímetros e milímetros.
+-----/
+
+#include <stdio.h>
+
+int main()
+{
+    int valor;
+    printf("Digite o valor em metros: ");
+    scanf ("%d",&valor);
+    printf("Valor em metros: ", valor);
+
+    return 0;
+}
+
